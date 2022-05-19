28742fe1d7040ae7cf161ee827b7212f468a9320
Manuel Bilbao <bilbaomanuel98@gmail.com>
2022-05-19 17:13:17 -0300

    Added 401 errors

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
 from app.endpoints import examples
+from app.endpoints.base import response_codes
 
 
 router = APIRouter()
-router.include_router(examples.router)
+router.include_router(examples.router, responses = {401: response_codes[401]})

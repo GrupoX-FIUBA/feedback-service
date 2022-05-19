724b8812895d5976331494a242e0d8f63f7ac3b6
Manuel Bilbao <bilbaomanuel98@gmail.com>
2022-05-19 17:15:47 -0300

    Removed example model

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,2 @@
 from app.db.base_class import Base  # noqa
-from app.models.examples import Example  # noqa
 from app.models.qualifications import Qualification  # noqa
--- app/models/examples.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-
-from app.db.base_class import Base
-
-
-class Example(Base):
-    id = Column(Integer, primary_key = True, index = True)
-
-    name = Column(String)
